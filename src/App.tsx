e259bd96fc6ff9613459ead13ea0bfeec4652de7
KieenVux <40339436+KieenVux@users.noreply.github.com>
2021-12-10 11:14:05 +0700

    Third_commit

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Vite Documents avc
+            Vite Documents
           </a>
         </p>
       </header>
