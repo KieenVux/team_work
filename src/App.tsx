d046f0be6d71b5545045735e723967e8284caaf7
tienpvse1 <tienpvse@fpt.edu.vn>
2021-12-12 10:41:43 +0700

    resolve(rules): add code rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
-import { useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
   // this is tien branch
   return (
     <div className="App">
